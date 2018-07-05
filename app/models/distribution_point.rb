92a28c21670abe6294319b0810161c9ce31a4bf9
Rodrigo Serradura <rodrigo@ysimplicity.com>
2018-07-05 18:07:39 -0300

    Test DistributionPoint model validations

--- app/models/distribution_point.rb
+++ app/models/distribution_point.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 class DistributionPoint < ApplicationRecord
+  validate :with_params_schema_validation
+
+  def with_params_schema_validation
+    result = DistributionPoints::BaseSchema.new.call(
+      attributes.except('id', 'created_at', 'updated_at').symbolize_keys
+    )
+
+    return if result.success?
+
+    result.errors.each { |attribute, error| errors.add(attribute, error) }
+  end
 end
--- spec/models/distribution_point_spec.rb
+++ spec/models/distribution_point_spec.rb
@@ -1,5 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe DistributionPoint, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validations' do
+    let(:attributes) { Hash[origin: orig, destination: dest, distance: dist] }
+
+    subject { described_class.new(attributes) }
+
+    context 'with valid data' do
+      let(:orig) { 'A' }
+      let(:dest) { " B\n\r" }
+      let(:dist) { 100_000 }
+
+      it { is_expected.to be_valid }
+    end
+
+    context 'with invalid data' do
+      let(:orig) { '' }
+      let(:dest) { " \n\r" }
+      let(:dist) { '1.' }
+
+      it { is_expected.to_not be_valid }
+
+      it { expect(subject.tap(&:valid?).errors).to include :origin }
+      it { expect(subject.tap(&:valid?).errors).to include :destination }
+      it { expect(subject.tap(&:valid?).errors).to include :distance }
+    end
+
+    it 'invokes DistributionPoints::BaseSchema' do
+      data = { origin: 'A', destination: 'B', distance: 1 }
+      result = double
+      schema = double
+
+      expect(result).to receive(:success?).and_return(true)
+
+      expect(schema).to receive(:call).with(data).and_return(result)
+
+      allow(DistributionPoints::BaseSchema).to receive(:new).and_return(schema)
+
+      described_class.new(origin: 'A', destination: 'B', distance: 1).valid?
+    end
+  end
 end
