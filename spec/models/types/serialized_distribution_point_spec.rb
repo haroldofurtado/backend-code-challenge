0608f0f75ff8c11494dfef5787c7e2c0dec6aae1
Rodrigo Serradura <rodrigo@ysimplicity.com>
2018-07-05 17:05:23 -0300

    Move Types::SerializedDistributionPoint spec for a new file

---
+++ spec/models/types/serialized_distribution_point_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe Types::SerializedDistributionPoint, type: :model do
+  context 'with invalid input' do
+    it 'raises a constraint error' do
+      invalid_data = [nil, '', 'A', 'A B', 'A B 1.']
+
+      only_errors = invalid_data.all? do |data|
+                      described_class[data]
+                    rescue Dry::Types::ConstraintError
+                      true
+                    end
+
+      expect(only_errors).to be_truthy
+    end
+  end
+
+  context 'with valid input' do
+    it 'returns the input as the output' do
+      valid_data = [
+        'A B 1',
+        'C D 2.1',
+        'E F 10.1',
+        'G H 10.11',
+      ]
+
+      output = valid_data.map &described_class.method(:call)
+
+      expect(output).to contain_exactly(*valid_data)
+    end
+  end
+end
--- spec/models/types_spec.rb
+++ spec/models/types_spec.rb
@@ -5,35 +5,4 @@ RSpec.describe Types, type: :model do
     expect(described_class)
       .to be_a_kind_of Dry::Types::BuilderMethods
   end
-
-  describe Types::SerializedDistributionPoint do
-    context 'with invalid input' do
-      it 'raises a constraint error' do
-        invalid_data = [nil, '', 'A', 'A B', 'A B 1.']
-
-        only_errors = invalid_data.all? do |data|
-                        described_class[data]
-                      rescue Dry::Types::ConstraintError
-                        true
-                      end
-
-        expect(only_errors).to be_truthy
-      end
-    end
-
-    context 'with valid input' do
-      it 'returns the input as the output' do
-        valid_data = [
-          'A B 1',
-          'C D 2.1',
-          'E F 10.1',
-          'G H 10.11',
-        ]
-
-        output = valid_data.map{ |data| described_class[data] }
-
-        expect(output).to contain_exactly(*valid_data)
-      end
-    end
-  end
 end
