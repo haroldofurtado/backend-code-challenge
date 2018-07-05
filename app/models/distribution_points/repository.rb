72db5674bd851c976c394e41fc198b0f52e62b3b
Rodrigo Serradura <rodrigo@ysimplicity.com>
2018-07-05 19:11:24 -0300

    Create DistributionPoints::Repository#create_or_update!

---
+++ app/models/distribution_points/repository.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+module DistributionPoints
+  class Repository
+    Result = Struct.new(:action, :output)
+
+    def initialize
+      @model = ::DistributionPoint
+      @base_schema = BaseSchema.new
+    end
+
+    def create_or_update!(data)
+      validate_result!(data) do |output|
+        result_to :create, @model.create!(output)
+      rescue ActiveRecord::RecordNotUnique
+        result_to :update, update_by_origin_and_destination(output)
+      end
+    end
+
+    private
+
+    def result_to(action, output)
+      Result.new(action, output).freeze
+    end
+
+    def validate_result!(data)
+      result = @base_schema.call(data)
+
+      raise Dry::Validation::InvalidSchemaError if result.failure?
+
+      yield result.output
+    end
+
+    def update_by_origin_and_destination(output)
+      scope = @model.where output.slice(:origin, :destination)
+
+      distance = output.slice(:distance)
+
+      scope.where.not(distance).update_all(distance)
+    end
+  end
+end
